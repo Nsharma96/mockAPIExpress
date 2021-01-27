9c2e14797b20b9b28ddc5828afefb21bd9ff3d11
Nikhil Sharma <ncksharma8@gmail.com>
2021-01-27 11:16:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express  = require('express');
+const fs = require('fs');
+
+const app = express();
+const rawDataAllProjects  = fs.readFileSync('./mock/projects.json')
+const projects = JSON.parse(rawDataAllProjects);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get("/projects",(req,res) => {
+    res.send(projects);
+});
+
+app.listen(3000);
